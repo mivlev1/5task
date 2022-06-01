3219c7e3c29cc993b6cdc4e486e1f03cd6bff36e
mivlev1 <makzoyn@gmail.com>
2022-06-02 02:17:30 +0300

    edit3

--- login.php
+++ login.php
@@ -4,13 +4,12 @@
 
 <head>
     <meta charset="utf-8"/>
-    <title>Kozin back5</title>
+    <title>5task</title>
     <script src="https://code.jquery.com/jquery-3.5.1.slim.min.js" integrity="sha384-DfXdz2htPH0lsSSs5nCTpuj/zy4C+OGpamoFVy38MVBnE+IbbVYUew+OrCXaRkfj" crossorigin="anonymous"></script>
     <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.1/dist/umd/popper.min.js" integrity="sha384-9/reFTGAW83EW2RDu2S0VKaIzap3H66lZH81PoYlFhbGU+6BZp6G7niu735Sk7lN" crossorigin="anonymous"></script>
     <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.min.js" integrity="sha384-B4gt1jrGC7Jh4AgTPSdUtOBvfO8shuf57BaghqFfPlYxofvL8/KUEfYiJOMMV+rV" crossorigin="anonymous"></script>
     <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css" integrity="sha384-JcKb8q3iqJ61gNV9KGb8thSsNjpSL0n8PARn9HuZOnIxN0hoP+VmmDGMN5t9UJ0Z" crossorigin="anonymous">
     <link href="style-login.css" rel="stylesheet">
-    <link href="https://faviconka.ru/ico/1/faviconka.ru_1_25078.ico" rel="icon">
 </head>
 
 <body>
@@ -19,8 +18,6 @@
     <div class="main  row mx-auto">
         <section id="form">
             <h2>Авторизация</h2>
-
-
             <?php
 
             /**
